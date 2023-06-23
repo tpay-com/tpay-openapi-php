7155ea1bd8520a0458630794ebe8414415d77c86
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2023-06-23 14:29:05 +0200

    Implemented JWT Signature verification, PaymentNotification marked as deprecated

--- Examples/Notifications/PaymentNotificationExample.php
+++ Examples/Notifications/PaymentNotificationExample.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace tpaySDK\Examples\Notifications;
 
 use tpaySDK\Examples\ExamplesConfig;
 use tpaySDK\Model\Objects\NotificationBody\BasicPayment;
+use tpaySDK\Utilities\TpayException;
+use tpaySDK\Webhook\JWSVerifiedPaymentNotification;
 use tpaySDK\Webhook\PaymentNotification;
 
 require_once '../ExamplesConfig.php';
@@ -14,20 +17,29 @@ class PaymentNotificationExample extends ExamplesConfig
      * Returns validated object with set parameters
      * @return BasicPayment
      */
-    public function checkPayment()
+    public function getVerifiedNotification()
     {
-        $NotificationWebhook = new PaymentNotification();
-
-        return $NotificationWebhook->getNotification('TRUE', static::MERCHANT_CONFIRMATION_CODE);
+        //if isProd == false -> use sandbox credentials.
+        $isProd = true;
+        $NotificationWebhook = new JWSVerifiedPaymentNotification(static::MERCHANT_CONFIRMATION_CODE, $isProd);
+        return $NotificationWebhook->getNotification();
     }
 
 }
-$notification = (new PaymentNotificationExample)->checkPayment();
-var_dump($notification->tr_id->getValue());
+
+try {
+//if there is no exception - notification is checked and ready to use.
+    $notification = (new PaymentNotificationExample)->getVerifiedNotification();
+    var_dump($notification->tr_id->getValue());
 //The above example will check the notification and print the value of received tr_id field
 //You can access any notification field by $notification->fieldName
 
-$notificationArray = $notification->getNotificationAssociative();
-var_dump($notificationArray);
+    $notificationArray = $notification->getNotificationAssociative();
+    var_dump($notificationArray);
 //The above method will get the notification as an associative array and print its contents.
 //You can access notification field value by $notificationArray['fieldName']
+    die('TRUE');
+} catch (TpayException $exception) {
+    //handle your exception here
+    die('FALSE');
+}
