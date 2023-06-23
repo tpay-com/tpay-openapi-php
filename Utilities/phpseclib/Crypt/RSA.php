cfed2ff09deb7aac2d239a0258da1ac29890ecf7
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2023-06-23 16:48:27 +0200

    removed check if class exists

--- Utilities/phpseclib/Crypt/RSA.php
+++ Utilities/phpseclib/Crypt/RSA.php
@@ -4,16 +4,12 @@
 namespace tpaySDK\Utilities\phpseclib\Crypt;
 
 if (class_exists('phpseclib3\Crypt\RSA')) {
-    if (!class_exists('tpaySDK\Utilities\phpseclib\Crypt\RSA')) {
-        abstract class RSA extends \phpseclib3\Crypt\RSA
-        {
-        }
+    abstract class RSA extends \phpseclib3\Crypt\RSA
+    {
     }
 } elseif (class_exists('phpseclib\Crypt\RSA')) {
-    if (!class_exists('tpaySDK\Utilities\phpseclib\Crypt\RSA')) {
-        class RSA extends \phpseclib\Crypt\RSA
-        {
-        }
+    class RSA extends \phpseclib\Crypt\RSA
+    {
     }
 } else {
     throw new \RuntimeException('Cannot find supported phpseclib/phpseclib library');
--- Utilities/phpseclib/File/X509.php
+++ Utilities/phpseclib/File/X509.php
@@ -4,26 +4,22 @@
 namespace tpaySDK\Utilities\phpseclib\File;
 
 if (class_exists('phpseclib3\File\X509')) {
-    if (!class_exists('tpaySDK\Utilities\phpseclib\File\X509')) {
-        class X509 extends \phpseclib3\File\X509
+    class X509 extends \phpseclib3\File\X509
+    {
+        public function withSettings($publicKey, $hash, $signature)
         {
-            public function withSettings($publicKey, $hash, $signature)
-            {
-                return $publicKey->withHash($hash)->withPadding($signature);
-            }
+            return $publicKey->withHash($hash)->withPadding($signature);
         }
     }
 } elseif (class_exists('phpseclib\File\X509')) {
-    if (!class_exists('tpaySDK\Utilities\phpseclib\File\X509')) {
-        class X509 extends \phpseclib\File\X509
+    class X509 extends \phpseclib\File\X509
+    {
+        public function withSettings($publicKey, $hash, $signature)
         {
-            public function withSettings($publicKey, $hash, $signature)
-            {
-                $publicKey->setHash($hash);
-                $publicKey->setSignatureMode($signature);
+            $publicKey->setHash($hash);
+            $publicKey->setSignatureMode($signature);
 
-                return $publicKey;
-            }
+            return $publicKey;
         }
     }
 } else {
