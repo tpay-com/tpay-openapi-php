8d2e67173df75303da7a98e5f55456000d4b7518
Kuba Werłos <werlos@gmail.com>
2023-08-16 16:44:51 +0200

    Add PHPUnit (#28)

---
+++ tests/Model/ModelsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tpay\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
+
+class ModelsTest extends TestCase
+{
+    /**
+     * @coversNothing
+     *
+     * @dataProvider dataModel
+     */
+    public function testModel($class)
+    {
+        self::assertTrue(class_exists($class) || interface_exists($class));
+    }
+
+    public static function dataModel()
+    {
+        $modelDirectory = realpath(__DIR__.'/../../Model');
+
+        /** @var SplFileInfo $fileInfo */
+        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($modelDirectory)) as $fileInfo) {
+            if (!$fileInfo->isFile()) {
+                continue;
+            }
+
+            $className = 'tpaySDK\\Model\\'.substr(
+                $fileInfo->getRealPath(),
+                strlen($modelDirectory) + 1,
+                -4
+            );
+            $className = str_replace('/', '\\', $className);
+
+            yield $className => [$className];
+        }
+    }
+}
