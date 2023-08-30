38edeb4517348a8277c4e130ef40fe8b2e3e54dd
Kuba Werłos <werlos@gmail.com>
2023-08-30 10:03:31 +0200

    Add test for file logging

---
+++ tests/Utilities/LoggerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tpay\Tests\OpenApi\Utilities;
+
+use PHPUnit\Framework\TestCase;
+use Tpay\OpenApi\Utilities\Logger;
+
+/**
+ * @covers \Tpay\OpenApi\Utilities\Logger
+ */
+class LoggerTest extends TestCase
+{
+    public function testDefaultLoggingToFile()
+    {
+        $logPath = __DIR__.'/../../src/Logs/log_'.date('Y-m-d').'.php';
+        if (file_exists($logPath)) {
+            unlink($logPath);
+        }
+
+        Logger::log('title', 'text');
+
+        $logContent = file_get_contents($logPath);
+
+        self::assertMatchesRegularExpression(self::getLogPattern(), $logContent);
+    }
+
+    /** @return string */
+    private static function getLogPattern()
+    {
+        return '/
+===========================
+title
+===========================
+\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}
+ip: Empty server REMOTE_ADDR
+text
+$/i';
+    }
+}
