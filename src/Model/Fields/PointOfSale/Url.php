d3a5bc22b523b6f495708a5da3788db11622261c
Adam Wysocki <adam.wysocki6431@gmail.com>
2023-11-23 14:22:28 +0100

    Change url regexp pattern to pass exotic domains

--- src/Model/Fields/PointOfSale/Url.php
+++ src/Model/Fields/PointOfSale/Url.php
@@ -12,5 +12,5 @@ class Url extends Field
     protected $name = __CLASS__;
     protected $type = self::STRING;
     protected $maxLength = 255;
-    protected $pattern = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)';
+    protected $pattern = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,12}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)';
 }
