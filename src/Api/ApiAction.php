43b8148aaedc8b19f9bc87f5aa4ae0bebfa349e2
Adam Wysocki <adam.wysocki6431@gmail.com>
2023-11-30 17:11:17 +0100

    Comment out source header

--- src/Api/ApiAction.php
+++ src/Api/ApiAction.php
@@ -146,7 +146,7 @@ class ApiAction
         }
 
         if ($this->clientName) {
-            $headers[] = 'X-Client-Source: '.$this->clientName;
+            // @phpcs:ignore $headers[] = 'X-Client-Source: '.$this->clientName;
         }
 
         Logger::log(
