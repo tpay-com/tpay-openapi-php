bc5afe314771255af0d266d077475acb5225816b
kGablo <krzysztof.gablo@tpay.com>
2024-05-28 11:06:44 +0200

    css url fix

--- examples/ExamplesConfig.php
+++ examples/ExamplesConfig.php
@@ -17,7 +17,7 @@ class ExamplesConfig
 
     public function __construct()
     {
-        Util::$libraryPath='/src/';
+        Util::$libraryPath = '/src/';
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
