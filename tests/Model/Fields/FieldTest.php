1a0b57077b6a6327abd0fb00d36cb48a02e07d1a
Aleksander Kąkol <aleksander.kakol@tpay.com>
2025-09-29 20:12:55 +0200

    DX-1226 Add test for Field

--- tests/Model/Fields/FieldTest.php
+++ tests/Model/Fields/FieldTest.php
@@ -2,13 +2,19 @@
 
 namespace Model\Fields;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use Tpay\OpenApi\Model\Fields\Field;
 
 class FieldTest extends TestCase
 {
     /**
      * @dataProvider validValueProvider
+     *
+     * @param mixed $property
+     * @param mixed $propertyValue
+     * @param mixed $valueToSet
      */
     public function testSetValueValid($property, $propertyValue, $valueToSet)
     {
@@ -23,6 +29,11 @@ class FieldTest extends TestCase
 
     /**
      * @dataProvider invalidValueProvider
+     *
+     * @param mixed $property
+     * @param mixed $propertyValue
+     * @param mixed $valueToSet
+     * @param mixed $expectedMessage
      */
     public function testSetValueInvalid($property, $propertyValue, $valueToSet, $expectedMessage)
     {
@@ -31,7 +42,7 @@ class FieldTest extends TestCase
         if ($property) {
             $this->setFieldProperty($field, $property, $propertyValue);
         }
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage($expectedMessage);
 
         $field->setValue($valueToSet);
@@ -43,42 +54,42 @@ class FieldTest extends TestCase
             [
                 'property' => null,
                 'propertyValue' => null,
-                'value' => 123
+                'value' => 123,
             ],
             [
                 'property' => 'maxLength',
                 'propertyValue' => 10,
-                'value' => 'test123'
+                'value' => 'test123',
             ],
             [
                 'property' => 'minLength',
                 'propertyValue' => 3,
-                'value' => 'test123'
+                'value' => 'test123',
             ],
             [
                 'property' => 'minimum',
                 'propertyValue' => 5,
-                'value' => 6
+                'value' => 6,
             ],
             [
                 'property' => 'maximum',
                 'propertyValue' => 12,
-                'value' => 10
+                'value' => 10,
             ],
             [
                 'property' => 'enum',
                 'propertyValue' => ['one', 'two'],
-                'value' => 'one'
+                'value' => 'one',
             ],
             [
                 'property' => 'type',
                 'propertyValue' => 'integer',
-                'value' => 10
+                'value' => 10,
             ],
             [
                 'property' => 'pattern',
                 'propertyValue' => '^([0-9]{1,3}\.){3}[0-9]{1,3}$',
-                'value' => '127.0.0.1'
+                'value' => '127.0.0.1',
             ],
         ];
     }
@@ -90,50 +101,50 @@ class FieldTest extends TestCase
                 'property' => 'maxLength',
                 'propertyValue' => 10,
                 'value' => 'test123test123',
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too long. Max allowed 10'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too long. Max allowed 10',
             ],
             [
                 'property' => 'minLength',
                 'propertyValue' => 3,
                 'value' => 'te',
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too short. Min required 3'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too short. Min required 3',
             ],
             [
                 'property' => 'minimum',
                 'propertyValue' => 5,
                 'value' => 3,
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too low. Min required 5'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is too low. Min required 5',
             ],
             [
                 'property' => 'maximum',
                 'propertyValue' => 5,
                 'value' => 10,
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field exceeds the limit. Max allowed 5'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field exceeds the limit. Max allowed 5',
             ],
             [
                 'property' => 'enum',
                 'propertyValue' => ['one', 'two'],
                 'value' => 'three',
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is invalid. Should be one of Array'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is invalid. Should be one of Array',
             ],
             [
                 'property' => 'type',
                 'propertyValue' => 'integer',
                 'value' => '',
-                'expectedMessage' => 'Value type of field Tpay\OpenApi\Model\Fields\Field is invalid. Should be integer type'
+                'expectedMessage' => 'Value type of field Tpay\OpenApi\Model\Fields\Field is invalid. Should be integer type',
             ],
             [
                 'property' => 'pattern',
                 'propertyValue' => '^([0-9]{1,3}\.){3}[0-9]{1,3}$',
                 'value' => 'te.st.12.3',
-                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is invalid. Should match ^([0-9]{1,3}\.){3}[0-9]{1,3}$ pattern'
+                'expectedMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Field is invalid. Should match ^([0-9]{1,3}\.){3}[0-9]{1,3}$ pattern',
             ],
         ];
     }
 
     protected function setFieldProperty($object, $property, $value)
     {
-        $ref = new \ReflectionClass($object);
+        $ref = new ReflectionClass($object);
         $prop = $ref->getProperty($property);
         $prop->setAccessible(true);
         $prop->setValue($object, $value);
