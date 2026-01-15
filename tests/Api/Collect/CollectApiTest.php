c6ec264a76cf35820d7342b2e645b8ff2c74cfa1
Rafał Grabosz <r.grabosz@rgisoft.pl>
2026-01-15 10:02:00 +0100

    Adds tests for CollectApi::addBankAccount method

---
+++ tests/Api/Collect/CollectApiTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Tpay\Tests\OpenApi\Api\Collect;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Tpay\OpenApi\Api\Collect\CollectApi;
+use Tpay\OpenApi\Model\Fields\Token\AccessToken;
+use Tpay\OpenApi\Model\Objects\Authorization\Token;
+use Tpay\Tests\OpenApi\Mock\CurlMock;
+use UnexpectedValueException;
+
+/**
+ * @covers \Tpay\OpenApi\Api\Collect\CollectApi
+ */
+class CollectApiTest extends TestCase
+{
+    /** @dataProvider validBankAccountData */
+    public function testAddingValidBankAccount($accountNumber, $ownerName, $additionalInformation)
+    {
+        $collectApi = $this->createCollectApiWithCurlMock('ok');
+
+        $result = $collectApi->addBankAccount($accountNumber, $ownerName, $additionalInformation);
+
+        self::assertSame('ok', $result);
+    }
+
+    /** @dataProvider invalidBankAccountData */
+    public function testAddingInvalidBankAccount($accountNumber, $ownerName, $additionalInformation, $exception, $exceptionMessage)
+    {
+        $collectApi = $this->createCollectApiWithCurlMock('error');
+
+        $this->expectException($exception);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        $collectApi->addBankAccount($accountNumber, $ownerName, $additionalInformation);
+    }
+
+    public function validBankAccountData()
+    {
+        yield 'Valid bank account' => [
+            'accountNumber' => 'PL12345678901234567890123456',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Konto firmowe',
+        ];
+
+        yield 'Min length owner name' => [
+            'accountNumber' => 'PL00000000000000000000000000',
+            'ownerName' => 'Jan',
+            'additionalInformation' => 'Inf',
+        ];
+
+        yield 'Max length owner name' => [
+            'accountNumber' => 'PL99999999999999999999999999',
+            'ownerName' => str_repeat('A', 70),
+            'additionalInformation' => str_repeat('B', 70),
+        ];
+    }
+
+    public function invalidBankAccountData()
+    {
+        yield 'Empty account number' => [
+            'accountNumber' => '',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AccountNumber is too short. Min required 28',
+        ];
+
+        yield 'Invalid account number format - missing PL prefix' => [
+            'accountNumber' => '1234567890123456789012345678',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AccountNumber is invalid. Should match ^PL\d{26}$ pattern',
+        ];
+
+        yield 'Account number too short' => [
+            'accountNumber' => 'PL1234567890123456789012345',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AccountNumber is too short. Min required 28',
+        ];
+
+        yield 'Account number too long' => [
+            'accountNumber' => 'PL123456789012345678901234567',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AccountNumber is too long. Max allowed 28',
+        ];
+
+        yield 'Owner name too short' => [
+            'accountNumber' => 'PL12345678901234567890123456',
+            'ownerName' => 'Ja',
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\OwnerName is too short. Min required 3',
+        ];
+
+        yield 'Owner name too long' => [
+            'accountNumber' => 'PL12345678901234567890123456',
+            'ownerName' => str_repeat('A', 71),
+            'additionalInformation' => 'Konto firmowe',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\OwnerName is too long. Max allowed 70',
+        ];
+
+        yield 'Additional information too short' => [
+            'accountNumber' => 'PL12345678901234567890123456',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => 'Ko',
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AdditionalInformation is too short. Min required 3',
+        ];
+
+        yield 'Additional information too long' => [
+            'accountNumber' => 'PL12345678901234567890123456',
+            'ownerName' => 'Jan Kowalski',
+            'additionalInformation' => str_repeat('X', 71),
+            'exception' => InvalidArgumentException::class,
+            'exceptionMessage' => 'Value of field Tpay\OpenApi\Model\Fields\Collect\AdditionalInformation is too long. Max allowed 70',
+        ];
+    }
+
+    private function createCollectApiWithCurlMock($response)
+    {
+        $accessToken = $this->createMock(AccessToken::class);
+
+        $token = $this->createMock(Token::class);
+
+        $token->access_token = $accessToken;
+
+        $collectApi = new CollectApi($token, false);
+
+        CurlMock::setConsecutiveReturnedTransfers(json_encode($response));
+
+        return $collectApi;
+    }
+}
